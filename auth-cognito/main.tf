fa18e032c7204a0ef968dd8272008a26f49d4902
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-10-07 08:34:33 +0300

    auth-cognito: remove username and passwod policies

--- auth-cognito/main.tf
+++ auth-cognito/main.tf
@@ -13,18 +13,6 @@ resource "aws_cognito_user_pool" "userpool" {
 	email_configuration {
 		email_sending_account = "COGNITO_DEFAULT"
 	}
-
-	username_configuration {
-	  case_sensitive = true
-	}
-
-	password_policy {
-		minimum_length = 6
-		require_lowercase = true
-		require_uppercase = true
-		require_numbers = true
-		require_symbols = true
-	}
 }
 
 resource "aws_cognito_user_pool_client" "userpool_client" {
