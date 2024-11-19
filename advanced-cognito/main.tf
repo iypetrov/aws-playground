d3b00ec6ebb42d66de1c85510fe1a3bfcfa847f8
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-11-19 16:34:59 +0200

    advanced-cognito: init setup

---
+++ advanced-cognito/main.tf
@@ -0,0 +1,34 @@
+resource "aws_cognito_user_pool" "userpool" {
+	name = "userpool"
+	auto_verified_attributes = ["email"]
+	username_attributes = [ "email" ]
+
+	schema {
+		name = "Email"
+		attribute_data_type = "String"
+		mutable = true
+		developer_only_attribute = false
+	}
+
+	email_configuration {
+		email_sending_account = "COGNITO_DEFAULT"
+	}
+}
+
+resource "aws_cognito_user_pool_client" "userpool_client" {
+	name = "userpool-client"
+	user_pool_id = aws_cognito_user_pool.userpool.id
+	supported_identity_providers = [ "COGNITO" ]
+	explicit_auth_flows = [ "ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH" ]
+	generate_secret = false
+	prevent_user_existence_errors = "LEGACY"
+	id_token_validity = 1
+	access_token_validity = 1
+	refresh_token_validity = 30
+
+	token_validity_units {
+		id_token = "hours"
+	 	access_token = "hours"
+		refresh_token = "days"
+	}
+}
---
+++ advanced-cognito/outputs.tf
@@ -0,0 +1,3 @@
+output "userpool_client_id" {
+	value = aws_cognito_user_pool_client.userpool_client.id
+}
