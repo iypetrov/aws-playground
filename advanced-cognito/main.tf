250904bc36bbede44bde1afd3bf487e6d8fad1d2
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2024-11-20 17:30:44 +0200

    advanced-cognito: add some code from chatgpt for identity providers

--- advanced-cognito/main.tf
+++ advanced-cognito/main.tf
@@ -18,7 +18,7 @@ resource "aws_cognito_user_pool" "userpool" {
 resource "aws_cognito_user_pool_client" "userpool_client" {
   name                          = "userpool-client"
   user_pool_id                  = aws_cognito_user_pool.userpool.id
-  supported_identity_providers  = ["COGNITO"]
+  supported_identity_providers  = ["COGNITO", "Google", "Facebook"]
   explicit_auth_flows           = ["ALLOW_USER_SRP_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH"]
   generate_secret               = false
   prevent_user_existence_errors = "LEGACY"
@@ -30,5 +30,29 @@ resource "aws_cognito_user_pool_client" "userpool_client" {
     id_token      = "hours"
     access_token  = "hours"
     refresh_token = "days"
+  }
+}
+
+resource "aws_cognito_identity_provider" "google" {
+  user_pool_id  = aws_cognito_user_pool.userpool.id
+  provider_name = "Google" 
+  provider_type = "Google"
+
+  provider_details = {
+    client_id        = var.google_client_id
+    client_secret    = var.google_client_secret
+    authorize_scopes = "email profile openid"
+  }
+}
+
+resource "aws_cognito_identity_provider" "facebook" {
+  user_pool_id  = aws_cognito_user_pool.userpool.id
+  provider_name = "Facebook" 
+  provider_type = "Facebook"
+
+  provider_details = {
+    client_id        = var.facebook_client_id
+    client_secret    = var.facebook_client_secret
+    authorize_scopes = "email public_profile"
   }
 }
