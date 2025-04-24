0b5fd3188926b54ab7b1b34a31106d984c723795
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-24 10:26:47 +0300

    ecr-push-if-not-find: init

---
+++ ecr-push-if-not-find/README.md
@@ -0,0 +1,3 @@
+the goal of this project is:
+
+create ecr and if fail to pull image from there to trigger a lambda function that will pull the target image from GitLab OCI and will push it to ECR
---
+++ ecr-push-if-not-find/foo/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/ip812/foo/config"
+	"github.com/ip812/foo/logger"
+)
+
+func Handler(ctx context.Context, event interface{}) (interface{}, error) {
+	log := logger.Get(ctx)
+	log.Info("%v", event)
+	return event, nil
+}
+
+func main() {
+	ctx := context.Background()
+	cfg := config.New()
+	log := logger.New(cfg)
+	ctx = log.Inject(ctx)
+	ctx = config.Inject(ctx, *cfg)
+
+	lambda.StartWithOptions(Handler, lambda.WithContext(ctx))
+}
