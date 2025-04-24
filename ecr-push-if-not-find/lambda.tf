3716cac2ccdbf5a0b311f15b1b73c0f64f7b8b7b
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-04-24 10:48:43 +0300

    ecr-push-if-not-find: use latest tag for foo lambda

--- ecr-push-if-not-find/lambda.tf
+++ ecr-push-if-not-find/lambda.tf
@@ -20,7 +20,7 @@ resource "aws_iam_role_policy_attachment" "foo_policy" {
 resource "aws_lambda_function" "foo_function" {
   function_name = "foo-notifier"
   timeout       = 60
-  image_uri = "678468774710.dkr.ecr.eu-west-2.amazonaws.com/playground/foo:ebeed25"
+  image_uri = "678468774710.dkr.ecr.eu-west-2.amazonaws.com/playground/foo:944d25d"
   package_type  = "Image"
   role          = aws_iam_role.foo_role.arn
   environment {
