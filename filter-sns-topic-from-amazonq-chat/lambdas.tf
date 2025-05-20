722c1af8763ad4c831f379f0fa9d1c13288024f9
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-20 16:04:07 +0300

    filter-sns-topic-amazonq-chat: attach lambda to the raw sns topic

---
+++ filter-sns-topic-from-amazonq-chat/lambdas.tf
@@ -0,0 +1,35 @@
+resource "aws_iam_role" "filter_sns_topic_from_amazonq_chat_function_role" {
+  name = "filter-sns-topic-from-amazonq-chat-function-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "filter_sns_topic_from_amazonq_chat_function_vpc_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  role       = aws_iam_role.filter_sns_topic_from_amazonq_chat_function_role.name
+}
+
+resource "aws_lambda_function" "filter_sns_topic_from_amazonq_chat_function" {
+  function_name = "filter-sns-topic-from-amazonq-chat"
+  timeout       = 5
+  image_uri     = "678468774710.dkr.ecr.eu-west-2.amazonaws.com/filter-sns-topic-from-amazonq-chat:1.0.2"
+  package_type  = "Image"
+  role          = aws_iam_role.filter_sns_topic_from_amazonq_chat_function_role.arn
+  vpc_config {
+    subnet_ids         = [aws_subnet.public_subnet_a.id, aws_subnet.public_subnet_b.id]
+    security_group_ids = [aws_security_group.vm_sg.id]
+  }
+  environment {
+    variables = {
+      APP_ENV = "prod"
+    }
+  }
+}
--- filter-sns-topic-from-amazonq-chat/sns.tf
+++ filter-sns-topic-from-amazonq-chat/sns.tf
@@ -2,6 +2,20 @@ resource "aws_sns_topic" "raw_alb_notifications" {
   name = "raw-alb-notifications"
 }
 
+resource "aws_lambda_permission" "allow_sns_invoke_lambda" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.filter_sns_topic_from_amazonq_chat_function.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.raw_alb_notifications.arn
+}
+
+resource "aws_sns_topic_subscription" "lambda_sub" {
+  topic_arn = aws_sns_topic.raw_alb_notifications.arn
+  protocol  = "lambda"
+  endpoint  = aws_lambda_function.filter_sns_topic_from_amazonq_chat_function.arn
+}
+
 resource "aws_sns_topic" "alb_notifications" {
   name = "alb-notifications"
 }
