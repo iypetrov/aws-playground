dd2acd2978b508b8213310eec27f866d120b97a7
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-05-21 15:42:35 +0300

    filter-sns-topic-amazonq-chat: bump lambda to 1.4.1

--- filter-sns-topic-from-amazonq-chat/lambdas.tf
+++ filter-sns-topic-from-amazonq-chat/lambdas.tf
@@ -31,7 +31,7 @@ resource "aws_iam_role_policy" "filter_sns_topic_from_amazonq_chat_function_sns_
 resource "aws_lambda_function" "filter_sns_topic_from_amazonq_chat_function" {
   function_name = "filter-sns-topic-from-amazonq-chat"
   timeout       = 900
-  image_uri     = "678468774710.dkr.ecr.eu-west-2.amazonaws.com/filter-sns-topic-from-amazonq-chat:1.4.0"
+  image_uri     = "678468774710.dkr.ecr.eu-west-2.amazonaws.com/filter-sns-topic-from-amazonq-chat:1.4.1"
   package_type  = "Image"
   role          = aws_iam_role.filter_sns_topic_from_amazonq_chat_function_role.arn
   environment {
