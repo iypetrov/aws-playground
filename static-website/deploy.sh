70a4cf6d76d5bcec9dcb07cbde2a9e3a48449cd5
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-21 13:44:12 +0300

    static-website: use cstate to create example web page
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- static-website/deploy.sh
+++ static-website/deploy.sh
@@ -6,6 +6,7 @@ BUCKET_NAME="$(terraform output -raw s3_bucket_name)"
 CLOUDFRONT_DISTRIBUTION_ID="$(terraform output -raw cloudfront_distribution_id)"
 CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME="$(terraform output -raw cloudfront_distribution_domain_name)"
 
-aws s3 cp static/ "s3://${BUCKET_NAME}/" --recursive
+hugo --config config.yml
+aws s3 cp public/ "s3://${BUCKET_NAME}/" --recursive
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
 echo "https://${CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME}"
