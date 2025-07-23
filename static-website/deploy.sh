9035beebc159bad3ec2cb2248b4d4fb81d56111d
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-23 09:39:46 +0300

    static-website: update the cloudfront not to use the default CA
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- static-website/deploy.sh
+++ static-website/deploy.sh
@@ -11,3 +11,4 @@ aws s3 cp public/ "s3://${BUCKET_NAME}/" --recursive
 aws cloudfront create-invalidation --distribution-id ${CLOUDFRONT_DISTRIBUTION_ID} --paths "/*"
 echo "https://${CLOUDFRONT_DISTRIBUTION_DOMAIN_NAME}"
 rm -rf public/
+rm .hugo_build.lock
---
+++ static-website/main.tf
@@ -0,0 +1,160 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket        = "ilia-petrov-website-bucket"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "website_bucket" {
+  bucket = aws_s3_bucket.website_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.website_bucket.bucket
+}
+
+resource "aws_s3_bucket_versioning" "website_versioning" {
+  bucket = aws_s3_bucket.website_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "website_bucket" {
+  bucket                  = aws_s3_bucket.website_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true 
+  ignore_public_acls      = true 
+  restrict_public_buckets = true 
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowCloudFrontAccess"
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for S3 bucket"
+}
+
+resource "aws_cloudfront_distribution" "website_distribution" {
+  depends_on = [
+    aws_acm_certificate_validation.cloudfront
+  ]
+
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-${aws_s3_bucket.website_bucket.bucket}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.website_bucket.bucket}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.cloudfront.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+}
+
+output "cloudfront_distribution_domain_name" {
+  value = aws_cloudfront_distribution.website_distribution.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  value = aws_cloudfront_distribution.website_distribution.id
+}
+
+resource "aws_route53_zone" "main" {
+  name = var.domain_name
+}
+
+resource "aws_acm_certificate" "cloudfront" {
+  provider = aws.us_east_1
+  domain_name       = "status.${var.domain_name}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cloudfront_validation" {
+  provider = aws.us_east_1
+  for_each = {
+    for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cloudfront" {
+  provider = aws.us_east_1
+  certificate_arn         = aws_acm_certificate.cloudfront.arn
+  validation_record_fqdns = [for record in aws_route53_record.cloudfront_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "cloudfront" {
+  name    = "status"
+  type    = "A"
+  zone_id = aws_route53_zone.main.zone_id
+
+  alias {
+    name                   = aws_cloudfront_distribution.website_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
