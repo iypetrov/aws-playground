2ce017744139025eede6599d3883bd9cbcd6fd73
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-07-30 14:31:10 +0300

    karpenter: add nat gateway
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- karpenter/vpc.tf
+++ karpenter/vpc.tf
@@ -5,6 +5,13 @@ resource "aws_vpc" "vpc" {
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = cidrsubnet(var.vpc_cidr, 8, 0)
+  availability_zone       = "${var.aws_region}a"
+  map_public_ip_on_launch = true
+}
+
 resource "aws_subnet" "private_subnet_a" {
   vpc_id            = aws_vpc.vpc.id
   cidr_block        = cidrsubnet(var.vpc_cidr, 8, 1)
@@ -17,8 +24,42 @@ resource "aws_subnet" "private_subnet_b" {
   availability_zone = "${var.aws_region}b"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+
+  depends_on = [aws_internet_gateway.igw]
+}
+
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
 }
 
 resource "aws_route_table_association" "private_rt_association_a" {
