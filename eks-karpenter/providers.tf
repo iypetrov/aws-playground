a00595da88706e987d2d87124eaa751106b60a33
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-08-01 21:21:34 +0300

    eks-karpenter: okay it worked and i scaled from 50 to 0 pods (it created and destroyed 34 instaces)
    wow, that was cool
    
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- eks-karpenter/providers.tf
+++ eks-karpenter/providers.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.31.0"
-    }
     kubectl = {
       source  = "gavinbunney/kubectl"
       version = ">= 1.14.0"
@@ -25,18 +21,6 @@ provider "aws" {
   region     = var.aws_region
 }
 
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
-    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
-  }
-}
-
 provider "kubectl" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
@@ -44,7 +28,6 @@ provider "kubectl" {
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
     command     = "aws"
-    # This requires the awscli to be installed locally where Terraform is executed
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   }
 }
