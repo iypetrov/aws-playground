111b0604323acf6460afff10d381bba5e1b2c2c7
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-08-01 21:33:31 +0300

    eks-karpenter: split providers + verions into 2 files
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- eks-karpenter/providers.tf
+++ eks-karpenter/providers.tf
@@ -1,20 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.14.0"
-    }
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.14.0"
-    }
-  }
-}
-
 provider "aws" {
   access_key = var.aws_access_key_id
   secret_key = var.aws_secret_access_key
