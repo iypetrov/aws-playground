c9561f3294c8fbf65503db46d2dcb6c4b0f164a5
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-08-12 09:51:53 +0300

    eks-karpenter: move the vpc to its own file
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ eks-karpenter/vpc.tf
@@ -0,0 +1,52 @@
+locals {
+  vpc_cidr    = "10.4.0.0/16"
+  vpc_name    = "vpc-karpenter-v3"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.vpc_name
+  cidr = local.vpc_cidr
+
+  azs = [
+    "${var.aws_region}a",
+    "${var.aws_region}b",
+    "${var.aws_region}c"
+  ]
+
+  private_subnets = [
+    cidrsubnet(local.vpc_cidr, 8, 1),
+    cidrsubnet(local.vpc_cidr, 8, 2),
+    cidrsubnet(local.vpc_cidr, 8, 3)
+  ]
+
+  public_subnets = [
+    cidrsubnet(local.vpc_cidr, 8, 4),
+    cidrsubnet(local.vpc_cidr, 8, 5),
+    cidrsubnet(local.vpc_cidr, 8, 6)
+  ]
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb"                  = 1
+    "kubernetes.io/cluster/${local.eks_name}" = "owned"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb"         = 1
+    "kubernetes.io/cluster/${local.eks_name}" = "owned"
+    "karpenter.sh/discovery"                  = "${local.eks_name}"
+  }
+
+  tags = {
+    Name = local.vpc_name
+  }
+}
