7ded0c0ab46ba1f809c4deae6575a257a219ed3e
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-02-26 16:17:29 +0200

    cert-manager-with-aws-alb-controller: init
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

---
+++ cert-manager-with-aws-alb-controller/eks.tf
@@ -0,0 +1,38 @@
+locals {
+  k8s_name    = "cert-manager-with-aws-alb-controller"
+  k8s_version = "1.35"
+
+  k8s_addons = {
+    cert_manager = "v1.19.3-eksbuild.2"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.15.1"
+
+  name               = local.k8s_name
+  kubernetes_version = local.k8s_version
+
+  endpoint_public_access                   = true
+  enable_cluster_creator_admin_permissions = true
+
+  create_auto_mode_iam_resources = true
+  compute_config = {
+    enabled    = true
+    node_pools = ["general-purpose"]
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  addons = {
+    cert-manager = {
+      addon_version = local.k8s_addons.cert_manager
+    }
+  }
+
+  tags = {
+    Name = local.k8s_name
+  }
+}
