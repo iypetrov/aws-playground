0afd71d6994294db9bc4bda0a31439c25c792996
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2026-03-02 09:44:36 +0200

    istio-poc: add the setup with cert-manager + NLB
    Signed-off-by: Ilia Petrov <ilia.yavorov.petrov@gmail.com>

--- istio-poc/eks.tf
+++ istio-poc/eks.tf
@@ -3,7 +3,7 @@ locals {
   k8s_version = "1.35"
 
   k8s_addons = {
-    cert_manager             = "v1.19.3-eksbuild.2"
+    cert_manager = "v1.19.3-eksbuild.2"
   }
 }
 
@@ -34,5 +34,23 @@ module "eks" {
 
   tags = {
     Name = local.k8s_name
+  }
+}
+
+module "cert_manager_pod_identity" {
+  source  = "terraform-aws-modules/eks-pod-identity/aws"
+  version = "2.7.0"
+
+  name = "cert-manager"
+
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = [data.aws_route53_zone.this.arn]
+
+  associations = {
+    this = {
+      cluster_name    = local.k8s_name
+      namespace       = "cert-manager"
+      service_account = "cert-manager"
+    }
   }
 }
